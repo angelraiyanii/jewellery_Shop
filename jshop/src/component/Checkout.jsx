27ad7a5acd994428c0ca25ab825c436578335c92
angelraiyanii <araiyani764@rku.ac.in>
2025-05-19 22:19:33 +0530

    User database

--- jshop/src/component/Checkout.jsx
+++ jshop/src/component/Checkout.jsx
@@ -110,7 +110,7 @@ class CheckOut extends Component {
                 razorpay_order_id: response.razorpay_order_id,
                 razorpay_payment_id: response.razorpay_payment_id,
                 razorpay_signature: response.razorpay_signature,
-                receipt: orderDetails._id, // Pass the order ID as receipt
+                receipt: orderDetails._id, 
               }
             );
 
